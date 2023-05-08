3983c30b406280586fa64cd24a4892afb142ee65
Radoslaw Lazur <radoslawla@papayaglobal.com>
2023-05-08 10:04:44 +0200

    init to compare

---
+++ src/main/java/com/lsn/task/lsntask/LsnTaskApplication.java
@@ -0,0 +1,13 @@
+package com.lsn.task.lsntask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsnTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsnTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsn/task/lsntask/LsnTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsn.task.lsntask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsnTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
